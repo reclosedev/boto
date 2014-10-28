1855d9c4be74a704c5d0174ff06644a29d1041bd
Mikhail Ushanov <MiUshanov@croc.ru>
2014-10-28 23:22:08 +0300

    Some cleanup in VPCs.

--- boto/vpc/vpc.py
+++ boto/vpc/vpc.py
@@ -14,7 +14,7 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
 # ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
-# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
@@ -37,7 +37,7 @@ class VPC(TaggedEC2Object):
 
     def __repr__(self):
         return 'VPC:%s' % self.id
-    
+
     def endElement(self, name, value, connection):
         if name == 'vpcId':
             self.id = value
@@ -54,4 +54,3 @@ class VPC(TaggedEC2Object):
 
     def delete(self):
         return self.connection.delete_vpc(self.id)
-
