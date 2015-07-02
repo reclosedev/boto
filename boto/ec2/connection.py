4bfde40619865ba050d174f20d791b65e6ae869c
Roman Haritonov <rharitonov@asdco.ru>
2015-07-02 13:51:16 +0300

    Remove version hardcode

--- boto/ec2/connection.py
+++ boto/ec2/connection.py
@@ -824,7 +824,7 @@ class EC2Connection(AWSQueryConnection):
                       ebs_optimized=False, network_interfaces=None,
                       high_available=None,
                       root_device_name=None, public_addressing=None,
-                      virtualization_type=None, description=None, 
+                      virtualization_type=None, description=None,
                       private_dns_name=None):
         """
         Runs an image on EC2.
